5e7356a6ad5eee86553470fbf19e5c2ec5e11e69
kindiregg <jpriveraa7@gmail.com>
2024-11-22 19:08:45 -0600

    C: character dictionary

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ book_path = "books/frankenstein.txt"
 def main():
     text = book_text(book_path)
     word_count = get_word_count(text)
+    book_low = lowered_string(text)
     # prints word count of book to check everything was printed
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
     print(f"Contains {word_count} words.")
+    for char in get_character_dict(book_low):
+
+        print(f"")
 
 # takes file contents returns count
 def get_word_count(booktext):
@@ -14,5 +20,16 @@ def get_word_count(booktext):
 def book_text(path):
     with open(path) as f:
         return f.read()
-        
+    
+def get_character_dict(book_low):
+    char_count = {}
+    for c in book_low:
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+    return char_count            
+            
+def lowered_string(book):
+    return book.lower()  
 main()
